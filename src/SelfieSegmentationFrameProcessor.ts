feeab71136a08588871b19520600e288612489fb
pweglik <przemek.weglik@onet.pl>
2024-05-27 21:40:21 +0200

    Add blur for entire image - comment out rest

--- src/SelfieSegmentationFrameProcessor.ts
+++ src/SelfieSegmentationFrameProcessor.ts
@@ -1,74 +1,105 @@
-import { AlphaType, ColorType, Skia, SkRect } from '@shopify/react-native-skia';
+import {
+  AlphaType,
+  ColorType,
+  Skia,
+  SkRect,
+  TileMode,
+} from '@shopify/react-native-skia';
+import { useCallback } from 'react';
 import { useTensorflowModel } from 'react-native-fast-tflite';
 import { Frame, useSkiaFrameProcessor } from 'react-native-vision-camera';
 import { useResizePlugin } from 'vision-camera-resize-plugin';
 
 export const useFrameSelfieSegmentation = () => {
-  const tf = useTensorflowModel(require('../assets/selfie_segmenter.tflite'));
+  const tf = useTensorflowModel(
+    require('../assets/selfie_segmenter.tflite'),
+    'default'
+  );
   const { resize } = useResizePlugin();
 
-  const segment = (frame: Frame) => {
-    'worklet';
-    if (tf.state !== 'loaded' || tf.model === undefined) {
-      return null;
-    }
+  console.log(tf);
 
-    const resizedFrame = resize(frame, {
-      scale: {
-        width: 256,
-        height: 256,
-      },
-      crop: {
-        x: 0,
-        y: 0,
-        width: frame.width,
-        height: frame.height,
-      },
-      pixelFormat: 'rgb',
-      dataType: 'float32',
-    });
-    const output: any[] = tf.model.runSync([resizedFrame]);
+  const segment = useCallback(
+    (frame: Frame) => {
+      'worklet';
+      if (tf.state !== 'loaded' || tf.model === undefined) {
+        console.log('MODEL not loaded');
+        return null;
+      }
 
-    return output[0];
-  };
+      const resized = resize(frame, {
+        scale: {
+          width: 256,
+          height: 256,
+        },
+        crop: {
+          x: 0,
+          y: 0,
+          width: frame.width,
+          height: frame.height,
+        },
+        pixelFormat: 'rgb',
+        dataType: 'float32',
+      });
 
-  const frameProcessor = useSkiaFrameProcessor((frame) => {
-    'worklet';
-    const mask = segment(frame);
+      const output: any[] = tf.model.runSync([resized]);
 
-    if (mask == null) {
-      return;
-    }
+      return output[0];
+    },
+    [tf, resize]
+  );
 
-    const data = Skia.Data.fromBytes(mask);
+  const frameProcessor = useSkiaFrameProcessor(
+    (frame) => {
+      'worklet';
 
-    const maskImage = Skia.Image.MakeImage(
-      {
-        width: 256,
-        height: 256,
-        alphaType: AlphaType.Opaque,
-        colorType: ColorType.Gray_8,
-      },
-      data,
-      256
-    );
-    if (maskImage == null) {
-      return;
-    }
+      const paintBlur = Skia.Paint();
+      const filterBlur = Skia.ImageFilter.MakeBlur(
+        50,
+        50,
+        TileMode.Clamp,
+        null
+      );
+      paintBlur.setImageFilter(filterBlur);
+      frame.render(paintBlur);
 
-    const srcRect: SkRect = { x: 0, y: 0, width: 256, height: 256 };
+      // const mask = segment(frame);
 
-    const dstRect: SkRect = {
-      x: 0,
-      y: 0,
-      width: frame.width,
-      height: frame.height,
-    };
+      // if (mask == null) {
+      //   return;
+      // }
 
-    const emptyPaint = Skia.Paint();
+      // const data = Skia.Data.fromBytes(mask);
 
-    frame.drawImageRect(maskImage, srcRect, dstRect, emptyPaint);
-  }, []);
+      // const maskImage = Skia.Image.MakeImage(
+      //   {
+      //     width: 256,
+      //     height: 256,
+      //     alphaType: AlphaType.Opaque,
+      //     colorType: ColorType.Gray_8,
+      //   },
+      //   data,
+      //   256
+      // );
+      // if (maskImage == null) {
+      //   return;
+      // }
+
+      // const srcRect: SkRect = { x: 0, y: 0, width: 256, height: 256 };
+
+      // const dstRect: SkRect = {
+      //   x: 0,
+      //   y: 0,
+      //   width: frame.width,
+      //   height: frame.height,
+      // };
+
+      // const emptyPaint = Skia.Paint();
+
+      // frame.drawImageRect(maskImage, srcRect, dstRect, emptyPaint);
+    },
+    [segment]
+  );
 
   return { frameProcessor };
 };
